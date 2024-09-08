6a01a66221a36d929fca1efa3b061a27a793d149
masashi katsumata <wf9a5m75@gmail.com>
2024-09-07 17:01:44 -0700

    ready for study class

---
+++ app/eurf-importer.js
@@ -0,0 +1,84 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Writable } = require('node:stream');
+const csv = require('csv-parser');
+const { pipeline } = require('node:stream/promises');
+
+export class EurfImporter extends Writable {
+  tableName = '';
+  conn = null;
+
+  constructor(mysql, tableName) {
+    super({
+      objectMode: true,
+    });
+    this.tableName = tableName;
+    this.conn = mysql;
+  }
+
+  async _write(chunk, _, callback) {
+    if (chunk['支所等'] === '合計' || chunk['性別'] === '合計' || chunk['年齢'] === '総数') {
+      callback();
+      return;
+    }
+    await this.conn.query(`
+      insert into ${this.tableName}
+      (yyyymm,lg_code,kobe_code,area,branch,sex,age,value)
+      values (?,?,?,?,?,?,?,?)
+    `, [
+      chunk['対象年月'],
+      chunk['区CD'],
+      chunk['行政区CD'],
+      chunk['区名'],
+      chunk['支所等'],
+      chunk['性別'],
+      chunk['年齢'],
+      chunk['値']
+    ]);
+      
+    callback();
+  }
+
+  static _createTable = async (conn, tableName) => {
+    await conn.query(`DROP TABLE IF EXISTS ${tableName};`)
+    await conn.query(`CREATE TABLE ${tableName} (
+  eurf_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  yyyymm VARCHAR(6),
+  lg_code VARCHAR(5),
+  kobe_code VARCHAR(5),
+  area VARCHAR(255),
+  branch VARCHAR(255),
+  sex VARCHAR(2),
+  age INT,
+  value INT
+)`);
+  }
+
+  static fromCsv = async (params) => {
+    const filename = params.dirent.name;
+    const [year] = path.basename(filename).split(/[\-\.]/);
+    const tableName = `eurf310005_${year}`;
+  
+    const reader = fs.createReadStream(`${params.dirent.parentPath}/${filename}`);
+    const parser = csv();
+    const csvImporter = new EurfImporter(params.conn, tableName);
+  
+    // テーブルの作成
+    await EurfImporter._createTable(params.conn, tableName);
+  
+    // トランザクションの開始
+    // await params.conn.query('.....');
+  
+    await pipeline(
+      // CSVファイルから読み込む
+      reader,
+      // CSVファイルのパース
+      parser,
+      // データベースに取り込む
+      csvImporter,
+    );
+  
+    // コミットする
+    // await params.conn.query('....');
+  }
+}
---
+++ app/import.js
@@ -0,0 +1,64 @@
+const fs = require('node:fs');
+const mysql = require('mysql2/promise');
+const { EurfImporter } = require('./eurf-importer');
+require('dotenv').config();
+
+(async () => {
+
+  // データベースへの接続
+  const conn = await mysql.createConnection(
+    {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    }
+  );
+  console.log(`connected`);
+
+  // データベースの作成
+  await conn.query(`
+    CREATE DATABASE IF NOT EXISTS kobe
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci
+  `);
+
+  // データベースの選択
+  await conn.query(`use kobe`);
+
+  // ディレクトリ内からcsvファイルを探してインポートする
+  const dir = await fs.promises.opendir("../kobe");
+  for await (const dirent of dir) {
+
+    switch (true) {
+      case dirent.name.endsWith('eurf310005.csv'): {
+        // データファイルの取り込み
+        console.log(`import: ${dirent.name}`);
+        await EurfImporter.fromCsv({
+          conn,
+          dirent,
+        });
+        break;
+      }
+
+      // TODO: ここは生徒が作る
+      //
+      // case dirent.name === "000925835.csv": {
+      //   // 全国地方公共団体コードの取り込み
+      //   await importLgCodeCsvFile({
+      //     conn,
+      //     dirent,
+      //   });
+      //   break;
+      // }
+
+      default:
+        // Do nothing here
+        break;
+    }
+    
+    break;
+  }
+  
+  process.exit();
+})();
+
